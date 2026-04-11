d759960d1265eac5b79e82f9133c8ce43a124b13
Lohith Srikar <167197181+l-Srikar@users.noreply.github.com>
2026-04-11 23:06:09 +0530

    feat: add logging to file and recover from panics in main function

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"log/slog"
+	"os"
+	"runtime/debug"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -33,6 +36,17 @@ func (cw chanWriter) Write(p []byte) (int, error) {
 }
 
 func main() {
+	f, _ := os.Create("termophone-log.txt")
+	log.SetOutput(io.MultiWriter(os.Stderr, f))
+
+	defer func() {
+		if r := recover(); r != nil {
+			log.SetOutput(io.MultiWriter(os.Stderr, f)) // ensure it goes to file
+			log.Printf("PANIC: %v", r)
+			log.Printf("%s", debug.Stack())
+		}
+	}()
+
 	logCh := make(chan string, 32)
 	cw := chanWriter(logCh)
 
